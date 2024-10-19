987916d16176fd0ee1c0e77e17cf1f87bab5012f
Thomas Z McMahon <thomas.ziesemer@snhu.edu>
2024-10-19 09:46:34 -0400

    Module 6 complete baseline

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,30 @@
+export const trips = [
+        {
+            "code" : "123456",
+            "name" : "Gale Reef",
+            "length" : "4 nights / 5 days",
+            "start" : "2021-02-14T08:00:00Z",
+            "resort" : "Emerald Bay, 3 stars",
+            "perPerson" : "799.00",
+            "image" : "reef1.jpg",
+            "description" : "<p>Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p><p>Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet.</p>"
+        }, {
+            "code" : "1234567",
+            "name" : "Dawson's Reef",
+            "length" : "4 nights / 5 days",
+            "start" : "2021-02-14T08:00:00Z",
+            "resort" : "Blue Lagoon, 4 stars",
+            "perPerson" : "1199.00",
+            "image" : "reef2.jpg",
+            "description" : "<p>Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollic vel vestibulum.</p><p>second paragraphs are also fun but I don't want to type anymore</P>"
+        }, {
+            "code" : "12345678",
+            "name" : "Claire's Reef",
+            "length" : "4 nights / 5 days",
+            "start" : "2021-02-14T08:00:00Z",
+            "resort" : "Coral Sands, 5 stars",
+            "perPerson" : "1999.00",
+            "image" : "reef3.jpg",
+            "description" :"<p>Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollic vel vestibulum.</p><p>second paragraphs are also fun but I don't want to type anymore</P>"
+        }
+];
